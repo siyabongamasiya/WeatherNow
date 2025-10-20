dbd1c81aa4d8f8bfd1a98cd57af34dbb1927f6b3
siyabonga <siyabongakhanyile76@gmail.com>
2025-10-20 08:40:47 +0200

    mod:aligned connent on the css file

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <SettingsProvider>
         <LocationProvider>
-          <RouterProvider router={router} />
+          <RouterProvider router={router} /> 
         </LocationProvider>
       </SettingsProvider>
     </ThemeProvider>
